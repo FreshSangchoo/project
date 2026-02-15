fae9f31c84066ff4b47580eaca70d92b02921fc5
FreshSangchoo <aprldpdy289@naver.com>
2026-02-16 02:49:38 +0900

    260216 fix security

--- donzoom/frontend/src/config/FireBaseConfig.tsx
+++ donzoom/frontend/src/config/FireBaseConfig.tsx
@@ -1,12 +1,20 @@
 import firebase from '@react-native-firebase/app';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSUTJysXrGAo2kxrgyEAVNiiVcr4Xfj40',
-  authDomain: 'donzoom.firebaseapp.com',
-  projectId: 'donzoom',
-  storageBucket: 'donzoom.appspot.com',
-  messagingSenderId: '1005190129327',
-  appId: '1:1005190129327:android:5f3155457bc18b39c7965a',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 export const initializeFirebase = () => {
